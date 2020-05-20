9dd10c4d7f78d1839147db06ded60789ab26e329
prabeen04 <prabeen.strange@gmail.com>
2020-05-21 00:40:06 +0530

    server.ts file created with oak server

---
+++ server.ts
@@ -0,0 +1,7 @@
+import {Application, Router} from 'https://deno.land/x/oak/mod.ts';
+  
+const app = new Application();
+const router = new Router();
+const config = {port: 3333);
+await app.listen(port)
+
