be13e4d1d5d448778fe14500107eff1b12cda2ae
prabeen04 <prabeen.strange@gmail.com>
2020-05-24 12:21:32 +0530

    DB configuration

--- db.ts
+++ db.ts
@@ -1,8 +1,18 @@
 import { DATA_TYPES, Database, Model } from 'https://deno.land/x/denodb/mod.ts';
+import { config, DotenvConfig } from "https://deno.land/x/dotenv/mod.ts";
 
+type CONFIG = DotenvConfig & {
+  host: string;
+  username: string;
+  password: string;
+  database: string;
+}
+const DB_CONFIG: DotenvConfig = config({
+  path: "db.env"
+})
 const db = new Database('postgres', {
-  host: 'localhost',
-  username: 'deno',
-  password: 'deno',
-  database: 'deno',
+  host: DB_CONFIG.host,
+  username: DB_CONFIG.username,
+  password: DB_CONFIG.password,
+  database: DB_CONFIG.database,
 });
