27030ce7f54687f96709bd746588843ec12cbf60
prabeen04 <prabeen.strange@gmail.com>
2020-05-28 22:33:30 +0530

    UserModel created

--- modules/user/user.model.ts
+++ modules/user/user.model.ts
@@ -1,4 +1,20 @@
-import { Model } from 'https://deno.land/x/denodb/mod.ts';
+import { Model, DATA_TYPES } from 'https://deno.land/x/denodb/mod.ts';
+
 export class UserModel extends Model {
-  static tableName = "users"
+  static table = 'user';
+
+  static timestamps = true;
+
+  static fields = {
+    id: {
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: DATA_TYPES.STRING,
+    firstname: DATA_TYPES.STRING,
+    lastname: DATA_TYPES.STRING,
+    email: DATA_TYPES.STRING,
+    password: DATA_TYPES.TEXT,
+  };
+
 }
