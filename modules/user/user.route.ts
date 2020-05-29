1ef9d00674c64c3cabcda00d2ab867644ee4fb36
prabeen04 <prabeen.strange@gmail.com>
2020-05-30 00:57:07 +0530

    small stuff

--- modules/user/user.route.ts
+++ modules/user/user.route.ts
@@ -2,5 +2,6 @@ import { UserModel } from './user.model.ts'
 
 export async function getAllUsers(ctx: any) {
   const users = await UserModel.all()
+  console.log(users)
   ctx.response.body = { name: "test", email: "test@test.com" }
 }
