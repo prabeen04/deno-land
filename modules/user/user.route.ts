b3ed250e8488a14f96d7d6833253e09b4b05c8ae
prabeen04 <prabeen.strange@gmail.com>
2020-05-30 22:04:13 +0530

    adUser route

--- modules/user/user.route.ts
+++ modules/user/user.route.ts
@@ -5,3 +5,7 @@ export async function getAllUsers(ctx: any) {
   console.log(users)
   ctx.response.body = { name: "test", email: "test@test.com" }
 }
+export async function addUser(ctx: any) {
+  console.log(ctx)
+  ctx.response.body = ctx
+}
--- router.ts
+++ router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts';
-import { getAllUsers } from './modules/user/user.route.ts';
+import { getAllUsers, addUser } from './modules/user/user.route.ts';
 const router = new Router();
 
 router.get('/users/:id', context => {
   context.response.body = "user detail"
 })
 router.get("/users", getAllUsers)
+router.post("/users", addUser)
 
 export default router;
