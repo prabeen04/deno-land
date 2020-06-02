7aec10e9892ca9b5424999d7de6a77dd85802414
prabeen04 <prabeen.strange@gmail.com>
2020-06-02 20:23:36 +0530

    getUserById

--- modules/user/user.route.ts
+++ modules/user/user.route.ts
@@ -2,13 +2,12 @@ import { UserModel } from './user.model.ts'
 import { Context } from 'https://deno.land/x/oak/mod.ts';
 export async function getAllUsers(ctx: Context) {
   const users = await UserModel.all()
-  console.log(users)
   ctx.response.body = users
 }
 export async function getUserById(ctx: Context) {
-  const users = await UserModel.all()
-  console.log(users)
-  ctx.response.body = { name: "test", email: "test@test.com" }
+  const { id } = await ctx.request.body()
+  const user = await UserModel.where("id": id).get()
+  ctx.response.body = user
 }
 export async function addUser(ctx: Context) {
   const payload: any = ctx.request.body
