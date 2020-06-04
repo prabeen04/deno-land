24ee3d05994e7bd45b797d97ab05abed0cc4e001
prabeen04 <prabeen.strange@gmail.com>
2020-06-04 21:41:46 +0530

    try catch added in user route handlers

--- modules/user/user.route.ts
+++ modules/user/user.route.ts
@@ -2,18 +2,36 @@ import { UserModel } from './user.model.ts'
 import { Context, RouterContext } from 'https://deno.land/x/oak/mod.ts';
 
 export async function getAllUsers(ctx: RouterContext) {
-  const users = await UserModel.all()
-  ctx.response.body = users
+  try {
+    const users = await UserModel.all()
+    ctx.response.body = users
+
+  } catch (error) {
+    ctx.response.body = error
+
+  }
 }
+
 export async function getUserById(ctx: RouterContext) {
   console.log(ctx)
-  const { id } = await ctx.params
-  const user = await UserModel.where("id", id).get()
-  ctx.response.body = user
+  try {
+    const { id } = await ctx.params
+    const user = await UserModel.where("id", id).get()
+    ctx.response.body = user
+
+  } catch (error) {
+    ctx.response.body = error
+
+  }
 }
 
 export async function addUser(ctx: RouterContext) {
-  const payload: any = ctx.request.body
-  const user = await UserModel.create(payload)
-  ctx.response.body = user
+  try {
+    const payload: any = ctx.request.body
+    const user = await UserModel.create(payload)
+    ctx.response.body = user
+  } catch (error) {
+    ctx.response.body = error
+
+  }
 }
